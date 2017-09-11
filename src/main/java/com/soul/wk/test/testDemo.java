17b7c4b412cdb3bb10fde9db6cc65b68ea1d0d00
SoulEater <wangkun@stu.xidian.edu.cn>
2017-09-11 17:22:49 +0800

    init push

--- src/main/java/com/soul/wk/test/testDemo.java
+++ src/main/java/com/soul/wk/test/testDemo.java
@@ -5,5 +5,6 @@ public class testDemo {
     public static void main(String[] args) {
         System.out.println("init success!");
         System.out.println();
+        System.out.println();
     }
 }
