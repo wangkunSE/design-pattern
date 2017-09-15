79eac6b70d4fc7891e025181fc14047cd085b6cc
SoulEater <wangkun@stu.xidian.edu.cn>
2017-09-15 17:58:57 +0800

    String mapper

--- src/main/java/com/soul/wk/test/testDemo.java
+++ src/main/java/com/soul/wk/test/testDemo.java
@@ -2,9 +2,25 @@ package com.soul.wk.test;
 
 public class testDemo {
 
+    /**
+     * -Xms 10M -Xmn10M -Xmx 20M -XX:+PrintGCDetails
+     *
+     * @param args
+     */
+    private static final int _1MB = 1024 * 1024;
+
     public static void main(String[] args) {
-        System.out.println("init success!");
-        System.out.println();
-        System.out.println();
+
+        byte[] a, b, c, d;
+        a = new byte[_1MB / 4];
+        b = new byte[_1MB / 4];
+        d = new byte[4 * _1MB];
+        c = new byte[4 * _1MB];
+        System.out.println("llllllll");
+        c = null;
+        c = new byte[4 * _1MB];
+        System.out.println("------------");
+
+
     }
 }
